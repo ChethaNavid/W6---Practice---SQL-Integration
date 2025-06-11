33d29d43edf8009b95f9f891a13967014f4e5cf2
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-06-11 21:08:24 +0700

    articles retrieved successfully

---
+++ Start Code/back/src/repositories/sqlArticleRepository.js
@@ -0,0 +1,79 @@
+//
+//  This repository shall:
+//  - Connect to the database (using the pool provided by the database.js)
+// -  Perfrom the SQL querries to implement the bellow API
+//
+import pool from "../utils/database.js";
+
+// Get all articles
+export async function getArticles() {
+    // TODO
+    try {
+        const [rows] = await pool.query("SELECT * FROM articles");
+        return rows;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Get one article by ID
+export async function getArticleById(id) {
+    // TODO
+    try {
+        const [rows] = await pool.query("SELECT * FROM articles WHERE id = ?", [id]);
+        return rows[0];
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Create a new article
+export async function createArticle(article) {
+    // TODO
+    const { title, content, journalist, category} = article;
+
+    try {
+        const [rows] = await pool.query(
+            'INSERT INTO articles(title, content, journalist, category) VALUES (?, ?, ?, ?)',
+            [title, content, journalist, category]
+        );
+        return {id: rows.insertId, ...article};
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Update an article by ID
+export async function updateArticle(id, updatedData) {
+    // TODO
+    const {title, content, journalist, category} = updatedData;
+    try {
+        const [rows] = await pool.query(
+            `UPDATE articles
+            SET 
+                title = ?,
+                content = ?,
+                journalist = ?,
+                category = ?
+            WHERE id = ?`, 
+            [
+                [title, content, journalist, category, id]
+            ]
+        );
+
+        return { affectedRows: rows.affectedRows };
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Delete an article by ID
+export async function deleteArticle(id) {
+    // TODO
+    try {
+        const [rows] = await pool.query('DELETE FROM articles WHERE id = ?', [id]);
+        return { affectedRows: rows.affectedRows };
+    } catch(error) {
+        console.error(error);
+    }
+}
